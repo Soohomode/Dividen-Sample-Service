1b9bf843ad58b59f306bb479b1b6e09b8eb226f8
soohomode <soohomode@gmail.com>
2024-07-31 15:11:41 +0900

    240731 스케줄러 01

--- src/main/java/com/sample/SampleApplication.java
+++ src/main/java/com/sample/SampleApplication.java
@@ -2,10 +2,12 @@ package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableScheduling
 public class SampleApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/sample/scheduler/TestScheduler.java
@@ -0,0 +1,14 @@
+package com.sample.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestScheduler {
+
+    @Scheduled(cron = "0/5 * * * * *")
+    public void test() {
+        System.out.println("now -> " + System.currentTimeMillis());
+    }
+
+}
