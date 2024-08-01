6009cb7ffe2e22b276ae20926fc402f6431cacae
soohomode <soohomode@gmail.com>
2024-08-01 18:19:03 +0900

    240801 레디스 캐시 구현 01

--- src/main/java/com/sample/SampleApplication.java
+++ src/main/java/com/sample/SampleApplication.java
@@ -2,12 +2,14 @@ package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class SampleApplication {
 
 	public static void main(String[] args) {
